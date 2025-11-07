3612b3e974e02537761ba3b4836acdabcbef0e74
Boris Moreno <boris_marco_moreno@hotmail.com>
2025-11-07 05:40:34 -0500

    deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import awsAmplify from 'astro-aws-amplify';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), mdx()],
     site: 'https://master.dfwmcx7ncrse1.amplifyapp.com',
-    output: 'server',
-    adapter: awsAmplify()
+    // output: 'server',
+    adapter: netlify()
 });
